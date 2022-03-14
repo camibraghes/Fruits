c22baa60dab1957c599a1f1ff104d645929f58ba
CamiBraghes <cameliabraghes@gmail.com>
2022-03-14 12:23:16 +0200

    Initial Commit

---
+++ Fruits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fruits
+//
+//  Created by Camelia Braghes on 13.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Camelia Braghes on 13.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
