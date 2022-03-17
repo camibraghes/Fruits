3bdf3774b6191cb5589637021fa9a49344d78db8
CamiBraghes <cameliabraghes@gmail.com>
2022-03-17 18:24:04 +0200

    Create an outstanding Settings View - Part 3

--- Fruits/App/SettingsView.swift
+++ Fruits/App/SettingsView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct SettingsView: View {
     
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding = false
+    
     var body: some View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack {
+                    
                     //Mark: Section 1
                     GroupBox(
                         label:
@@ -30,8 +33,39 @@ struct SettingsView: View {
                                 .font(.footnote)
                         }
                     }
-                    //Mark: Section 2
                     
+                    //Mark: Section 2
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush"))
+                    {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("If you wish, you can restart the application by toggle the switch in this box.That way it starts the onboarding process an you will see the welcome screen again")
+                            .padding(.vertical, 8)
+                            .font(.footnote)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .foregroundColor(.green)
+                                    .fontWeight(.bold)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(
+                            Color(UIColor.tertiarySystemBackground)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        )
+                        
+                    }
+
                     //Mark: Section 3
                     GroupBox(
                         label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
